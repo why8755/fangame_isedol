b0a813b94a92f89a394f6209937f6054d6d07b37
Boramuneo <91676959+Boramuneo@users.noreply.github.com>
2022-07-16 21:41:50 +0900

    Muneo_Dialog
    Start 씬에 추가해놨습니다.
    스크립트 파일 및 엑셀 임포터는 Z_Test 폴더 안에 있습니다.

---
+++ Assets/Z_Test/testDialog.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset]
+public class testDialog : ScriptableObject
+{
+	public List<DialogEntity> Sheet1;
+}
